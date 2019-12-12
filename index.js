b5b01b9a8c54828473dbc41b10c8aa0b268a1aca
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-12 15:48:10 +0100

    created a render which will create canvas element

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const { Engine, Render, Runner, World, Bodies } = Matter
 
 const engine = Engine.create()
 const { world } = engine
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600
+  }
+})
