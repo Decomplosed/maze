1e25b02bd5dfc1e8f2b20b7fbee093b4c8a6b19a
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-12 16:15:55 +0100

    removed shape const that we don't need anymore

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ const render = Render.create({
 Render.run(render)
 Runner.run(Runner.create(), engine)
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-})
-
-World.add(world, shape)
-
 // Walls
 
 const walls = [
