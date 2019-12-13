3a18dfa78c3ecdacc2a48e19316461e2036ec6dc
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-13 16:15:18 +0100

    detecting a win and added a win animation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter
 
-const cells = 10
+const cells = 6
 const width = 600
 const height = 600
 
@@ -131,6 +131,7 @@ horizontals.forEach((row, rowIndex) => {
       unitLength,
       5,
       {
+        label: 'wall',
         isStatic: true
       }
     )
@@ -150,6 +151,7 @@ verticals.forEach((row, rowIndex) => {
       5,
       unitLength,
       {
+        label: 'wall',
         isStatic: true
       }
     )
@@ -165,6 +167,7 @@ const goal = Bodies.rectangle(
   unitLength * .7,
   unitLength * .7,
   {
+    label: 'goal',
     isStatic: true
   }
 )
@@ -176,7 +179,10 @@ World.add(world, goal)
 const ball = Bodies.circle(
   unitLength / 2,
   unitLength / 2,
-  unitLength * .3
+  unitLength * .3,
+  {
+    label: 'ball'
+  }
 )
 
 World.add(world, ball)
@@ -199,4 +205,24 @@ document.addEventListener('keydown', event => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, { x: x - 5, y })
   }
+})
+
+// Win Condition
+
+Events.on(engine, 'collisionStart', event => {
+  event.pairs.forEach((collision) => {
+    const labels = ['ball', 'goal']
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      world.gravity.y = 1
+      world.bodies.forEach(body => {
+        if (body.label === 'wall') {
+          Body.setStatic(body, false)
+        }
+      })
+    }
+  })
 })
